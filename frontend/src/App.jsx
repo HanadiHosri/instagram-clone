d29630d918f8642aa0a590d42efb1159361ed8de
unknown <hanadi.hosri@gmail.com>
2024-04-09 09:28:01 +0300

    making sign up functional

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import "./styles/colors.css";
 import "./styles/utilities.css";
 import Auth from "./pages/Auth/index.jsx";
+import Profile from "./pages/profile/index.jsx"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/auth" element={<Auth />} />
-          
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/pages/Auth/components/Signup.jsx
+++ frontend/src/pages/Auth/components/Signup.jsx
@@ -4,8 +4,29 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 const Signup = () => {
-    const [credentials, setCredentials] = useState({email: "", password: "", username: ""});
+    const [credentials, setCredentials] = useState({email: "", password: "", name: ""});
     const navigate = useNavigate();
+
+    const register = async () => {
+        try {
+            const {email, password, name} = credentials;
+            const formData = new FormData();
+            formData.append('email', email);
+            formData.append('password', password);
+            formData.append('name', name);
+
+            const response = await axios.post (
+                "http://127.0.0.1:8000/api/register",
+                formData,
+            );
+            if (response.data.status ==="success") {
+                navigate("/profile");
+            }
+
+        } catch (error) {
+            console.error(error)
+        }
+    }
 
     return (
         <div className="page column center gap">
@@ -23,7 +44,7 @@ const Signup = () => {
                 type="text"
                 placeholder="Username"
                 onChange={(e) => {
-                    setCredentials({...credentials, username: e.target.value,});
+                    setCredentials({...credentials, name: e.target.value,});
                 }}
                 />
                 <input
@@ -35,14 +56,10 @@ const Signup = () => {
                 />
                 <p className="small-text">People who use our service may have uploaded your contact information to Instagram. Learn More</p>
                 <p className="small-text">By signing up, you agree to our Terms, Privacy Policy and Coocies Policy</p>
-                <button className="button primary-bg rounded white-text" 
-                onClick={async () => {
-                    
-                }}
-                >Sign up</button>
+                <button className="button primary-bg rounded white-text" onClick={register}>Sign up</button>
             </div>
             <div className="container">
-                <p>Have an account? Log in</p>
+                <p>Have an account? <a href="/login">Log in</a></p>
             </div>
         </div>
     )
