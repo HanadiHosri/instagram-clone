11291d92a1c6371daba4ce5cfdae6020e2acf852
unknown <hanadi.hosri@gmail.com>
2024-04-11 18:47:56 +0300

    adding static sidebar

---
+++ frontend/src/common/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import "../styles/sidebar.css";
+
+const Sidebar = () => {
+
+    return (
+        <div className="sidebar">
+            <h3>Instagram</h3>
+            <ul>
+                <li>Home</li>
+                <li>Profile</li>
+                <li>Log out</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Sidebar;
